c73a097268de037867c9e13e8b8e6c2cc862d469
Joel Jolly <128782382+withinJoel@users.noreply.github.com>
2024-05-13 14:47:36 +0530

    Update

--- Script/Commands.js
+++ Script/Commands.js
@@ -62,6 +62,8 @@ function runCmd(command, inMemory, fromInput) {
                 getBatteryInfo();
             } else if (commandName === "mouseposition") {
                 getMousePosition();
+            } else if (commandName.includes("youtube:")) {
+                searchYoutube(commandName);
             } else if (commandName === "createpassword") {
                 createPassword();
             } else if (commandName.includes("createqr:")) {
