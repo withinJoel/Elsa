db66324f62bad503f65903c87448ec20c64b2ea8
Joel Jolly <128782382+withinJoel@users.noreply.github.com>
2024-05-14 20:56:29 +0530

    Update

--- Script/Packages.js
+++ Script/Packages.js
@@ -588,9 +588,20 @@ function createPassword(length = 16) {
     echo(`Password: ${password}`);
 }
 
-// Encode
+// Decode
+function decode(input) {
+    const data = input.trim().replace(/^decode:\s*/i, '');
+    try {
+        const decoded = atob(data);
+        echo(`Base64 decoded: ${decoded}`);
+    } catch (error) {
+        echo(`Error decoding: ${error.message}`);
+    }
+}
+
+//Encode
 function encode(input) {
-    const data = input.trim().replace(/^encode:\b\s*/i, ''); // Use input instead of number
+    const data = input.trim().replace(/^encode:\s*/i, '');
     try {
         const encoded = btoa(data);
         echo(`Base64 encoded: ${encoded}`);
